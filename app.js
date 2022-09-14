4c543c4a49e47ca2d4b4c62181716dc8a2426d9b
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-09-14 18:49:30 -0300

    Ejercicios node.js

--- app.js
+++ app.js
@@ -1,5 +1,76 @@
-const saludos = require('./saludos')
+// function mostrarTema(tema){
+//   console.log(`Estoy aprendiendo ${tema}`)
+// }
 
-console.log(saludos.saludar('Andrés'))
+// setTimeout(mostrarTema, 1000, 'Node.js')
 
-console.log(saludos.saludarHolaMundo())
+
+// function mostrarTema(tema){
+//   console.log(`Estoy aprendiendo ${tema}`)
+// }
+
+// console.log('Antes de setImmediate()')
+
+// setImmediate(mostrarTema, 'Node.js') // la funcion tine la maxima prioridad
+
+
+// console.log('Despues de setImmediate()')
+
+// function mostrarTema(tema){
+//   console.log(`Estoy aprendiendo ${tema}`)
+// }
+// setInterval(mostrarTema, 1500, 'Node.js')
+
+// function sumar(a,b){
+//   console.log(a + b)
+// }
+// setInterval(sumar, 1500, 8, 2)
+
+const fs = require('fs')
+
+// Leer un archivo
+
+fs.readFile('index.html', 'utf-8', (err, contenido) => {
+  if(err){
+    throw err
+  }else {
+    console.log(contenido)
+  }
+  console.log('Mensaje')
+})
+
+// Cambiar nombre de un archivo
+
+fs.rename('index.html', 'main.html', (err) =>{
+  if(err){
+    throw err
+  }
+  console.log('Nombre cambiado exitosamente')
+})
+
+// Agregar contenido al final de un archivo.
+
+fs.appendFile('index.html', '<p>Contenido nuevo</p>', (err)=>{
+  if(err){
+    throw err
+  }
+  console.log('Agrego nuevo contenido')
+})
+
+// Remplazar todo el contendio del archivo
+
+fs.writeFile('index.html', 'Contenido Nuevo', (err) =>{
+  if (err){
+    throw err
+  }
+  console.log('Contenido remplazo exitosamente')
+})
+
+// Eliminar archivo
+
+fs.unlink('main.js', (err) =>{
+  if(err){
+    console.log(err)
+  }
+  console.log('Archivo eliminado correctamente')
+})
--- saludos.js
+++ saludos.js
@@ -7,10 +7,6 @@ function saludarHolaMundo(){
     return 'Hola Mundo'
 }
 
-
-// module.exports.saludar = saludar
-// module.exports.saludarHolaMundo = saludarHolaMundo
-
 module.exports = {
     saludar: saludar,
     saludarHolaMundo: saludarHolaMundo
